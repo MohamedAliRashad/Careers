0a97a358a8cc86085193ff06c2d791ea134a3e41
MohamedAliRashad <m.rashadnow@gmail.com>
2020-12-03 22:36:35 +0200

    Initial Commits

---
+++ main.py
@@ -0,0 +1,15 @@
+import yaml
+import json
+from jinja2 import Template
+
+if __name__ == "__main__":
+    index = "index.html"
+    with open("config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        print(json.dumps(config, indent=4))
+
+    with open("template.html", 'r') as f:
+        template = Template(f.read())
+    
+    with open("index.html", 'w') as f:
+        f.write(template.render(**config))
